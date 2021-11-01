1d77d0678522539906ace00131e749fdb6e0776e
ajbodine <abodine@sralab.org>
2021-11-01 10:33:10 -0500

    Add demo.R

---
+++ demo.R
@@ -0,0 +1,24 @@
+## Load library for mvrnorm function
+library(MASS)
+
+## Generate data
+data <- mvrnorm(n = 1000,
+                mu = c(15, 16),
+                Sigma = matrix(c(1, .7, .7, 1), nrow = 2, byrow = T)
+)
+data <- as.data.frame(data)
+colnames(data) <- c('x', 'y')
+
+## Perform the regression
+reg <- lm(y ~ x, data = data)
+
+## Create a scatterplot of the data
+plot(x = data$x, y = data$y,
+     main = "A plot of a Regression",
+     xlab = 'X', ylab = 'Y',
+     pch = 16,
+     col = rgb(200/255, 100/255, 20/255, .4)
+)
+
+## Add the regression line
+abline(reg, lty = 2)
