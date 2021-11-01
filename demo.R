ce6bf33d84fed4f5527a43f4b6510d25b68c3480
ajbodine <abodine@sralab.org>
2021-11-01 11:50:48 -0500

    reverting to demo.R to linear only and orange dots

--- demo.R
+++ demo.R
@@ -9,33 +9,16 @@ data <- mvrnorm(n = 1000,
 data <- as.data.frame(data)
 colnames(data) <- c('x', 'y')
 
-## Perform the regressions
+## Perform the regression
 reg <- lm(y ~ x, data = data)
-reg2 <- lm(y ~ x^2, data = data)
-reg3 <- lm(y ~ exp(x), data = data)
 
-## Create scatterplots of the data with regression lines
-par(mfrow = c(1, 3))
+## Create a scatterplot of the data
 plot(x = data$x, y = data$y,
-     main = "Linear",
+     main = "A plot of a Regression",
      xlab = 'X', ylab = 'Y',
      pch = 16,
      col = rgb(0/255, 0/255, 0/255, .4)
 )
+
+## Add the regression line
 abline(reg, lty = 2)
-
-plot(x = data$x, y = data$y,
-     main = "Quadratic",
-     xlab = 'X', ylab = 'Y',
-     pch = 16,
-     col = rgb(0/255, 0/255, 0/255, .4)
-)
-abline(reg2, lty = 2)
-
-plot(x = data$x, y = data$y,
-     main = "Logarithmic",
-     xlab = 'X', ylab = 'Y',
-     pch = 16,
-     col = rgb(0/255, 0/255, 0/255, .4)
-)
-abline(reg3, lty = 2)
